fbb65ca01dcf59f2880e229865129c3a2b947932
Danijel-James Wynyard <danijel@repasscloud.com>
2026-04-02 14:29:10 +1100

    stash

---
+++ copy-in.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+cp -r /Users/danijeljw/Developer/Aethon.JobImport/aethon-software-import-data/ /Users/danijeljw/Developer/Aethon/aethon-software-import-data/
+
---
+++ copy-out.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+rm -rf /Users/danijeljw/Developer/Aethon/aethon-software-import-data/.git
+cp -r /Users/danijeljw/Developer/Aethon/aethon-software-import-data/ /Users/danijeljw/Developer/Aethon.JobImport/aethon-software-import-data/
+
